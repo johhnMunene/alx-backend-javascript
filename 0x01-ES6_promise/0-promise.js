e37afbe13fcc9eb1e4024e7e2135f979c4efc1ac
johhnMunene <Edwardsjohnny64@gmail.com>
2024-04-10 10:27:18 -0500

    promise  updates

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,8 @@
 export default function getResponseFromAPI () {
   return new Promise((resolve, reject) => {
-    // You can make your API call or perform any asynchronous operation here
     setTimeout(() => {
       const responseData = { message: 'Successfully fetched data from API' }
       resolve(responseData)
-    }, 2000) // Simulating a 2-second delay
+    }, 2000)
   })
 }
