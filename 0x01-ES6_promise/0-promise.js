701ad827b4d9ca37b506a5f533786d49d9b876f9
johhnMunene <Edwardsjohnny64@gmail.com>
2024-04-10 10:24:25 -0500

    promise  updates

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,9 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // You can make your API call or perform any asynchronous operation here
-        
-        // For example, let's simulate an API call with a setTimeout
-        setTimeout(() => {
-            // Simulating a successful response
-            const responseData = { message: "Successfully fetched data from API" };
-            
-            // Resolve the promise with the data
-            resolve(responseData);
-            
-            // If there's an error, you can reject the promise
-            // reject(new Error("Failed to fetch data from API"));
-        }, 2000); // Simulating a 2-second delay
-    });
+export default function getResponseFromAPI () {
+  return new Promise((resolve, reject) => {
+    // You can make your API call or perform any asynchronous operation here
+    setTimeout(() => {
+      const responseData = { message: 'Successfully fetched data from API' }
+      resolve(responseData)
+    }, 2000) // Simulating a 2-second delay
+  })
 }
