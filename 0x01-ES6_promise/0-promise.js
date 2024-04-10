e74aecf85f76afdb9a3030139d09c908fb8bdbec
johhnMunene <Edwardsjohnny64@gmail.com>
2024-04-10 10:03:56 -0500

    promise  updates

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,5 @@
-let myPromise = new Promise(function getResponseFromAPI(myResolve,MyReject){
-    myResolve();
-    MyReject
-});
-myPromise.then(
-    function(value) { /* code if successful */ },
-    function(error) { /* code if some error */ }
-  );
+export default function getResponseFromAPI(){
+    return new Promise((Resolve,Reject)=>{
+        
+    }
+}
